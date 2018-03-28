a3863c777e8d7308319840a13dddde808d3a97e0
reshmaelzathampi <36693752+reshmaelzathampi@users.noreply.github.com>
2018-03-28 10:30:23 +0530

    Update add_faculty.php

--- add_faculty.php
+++ add_faculty.php
@@ -1,86 +1,79 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Untitled Document</title>
-</head>
-
-<body>
-<?php 
-
-include("mysql.php");
-if(isset($_GET['addfaculty']))
-{
-$result = mysql_query("insert into facultytbl values ('','$_GET[fname]','$_GET[fpost]','$_GET[facultyno]','$_GET[emailid]','$_GET[mobno]')");
-if($result==1)
-{
-$uid=mysql_query("select max(fid) from usertbl");
-$userid=mysql_fetch_row($uid);
-$_SESSION['fid']=$userid[0];
-$_SESSION['facultyname']=$_GET['fname'];
-header("location:user_account.php");
-}
-}
+<?php include('header.php');
+include("db.php");
+$obj=new db();
 ?>
-
-
-           <form method="get" action="#">
-					<table width="500" cellpadding="5" cellspacing="5">
-					<th>Create faculty Prifile</th>
-  <tr>
-    <td>faculty Name</td>
-    <td><input name="fname" type="text" id="uname" required/></td>
-  </tr>
-  
-  <tr>
-   <td>designation</td>
-   <td>
-<select name="fpost">
-<option value="HOD" selected>HOD</option>
-<option value="Proffessor">Proffessor</option>
-<option value="Ass.Proffessor">Ass.Proffessor</option>
-<option value="Asso.Proffessor">Asso.Proffessor</option>
-<option value="Ass.Teacher">Ass.teacher</option>
-</select></td>
-  </tr>
-  
-  <tr>
-    <td>faculty no</td>
-    <td><input name="facultyno" type="text" id="facid" required/></td>
-  </tr>
- 
-  <tr>
-    <td>Email Id</td>
-    <td><input name="emailid" type="text" id="emailid" required/></td>
-  </tr>
-
+<style>
+th{
+	background:#7FAEF7;
+	}
+</style>
+<h1 align="center" style="color:;">Staff Detail</h1>
+<table width="100%"><tr><td>
+<table align="center" style="margin-left:6em;">
 <tr>
-    <td>mobile no</td>
-    <td><input name="mobno" type="text" id="mobno" required/></td>
-  </tr>
-  
-  <tr>
-    <td>&nbsp;</td>
-    <td><input type="submit" name="addfaculty" id="addfaculty" class="button" value="submit" /></td>
-  </tr>
-</table> </form>
+<th style="width:5em;">STAFF ID</th>
+<th style="width:20em;">STAFF NAME</th>
+<th style="width:5em;">OPERATIONS</th>
+</tr>
+<?php
+$i=1;
+$sql1="SELECT * FROM staff WHERE status=1 ";
+$res1=$obj->exe($sql1);
+while($r1=mysqli_fetch_array($res1))
+{
+	if($i%2==0){
+?>
+<tr>
+<td style="width:5em; background:#827F7F;color:#FFFFFF;" align="center"><?php echo $r1[0];?></td>
+<td style="width:20em;background:#827F7F;color:#FFFFFF;" align="center"><?php echo $r1[1];?></td>
+<td style="width:5em;background:#827F7F;color:#FFFFFF;" align="center"><a href="delstaff.php?id=<?php echo $r1[0];?>">
+<input type="button" value="DELETE"></a>
+</td>
+</tr>
+<?php }
+else{
+?>
+<tr>
+<td style="width:5em; background:#FFFFFF;color:#000000;" align="center"><?php echo $r1[0];?></td>
+<td style="width:20em;background:#FFFFFF;color:#000000;" align="center"><?php echo $r1[1];?></td>
+<td style="width:5em;background:#FFFFFF;color:#000000;" align="center"><a href="delstaff.php?id=<?php echo $r1[0];?>">
+<input type="button" value="DELETE"></a>
+</td>
+</tr>
+<?php }
+$i++;
+} ?>
+</table>
+</td>
+<td align="center">
+<table align="center">
+<tr>
+<th colspan="2">ADD NEW STAFF</th>
+</tr><form action="addstaff.php" method="post">
+<tr style="height:3em;">
+<td><br />Name</td>
+<td><br /><input type="text" name="name" placeholder="Enter Staff Name" required style="width:15em;"></td>
+</tr>
+<tr style="height:3em;">
+<td><br />Designation</td>
+<td>
+<br />
+<select name="desig" required style="width:15em;">
+<option selected disabled>Choose Designation</option>
+<option value="Professor">Professor</option>
+<option value="Associate Professor">AssociateProfessor</option>
+<option value="Assistant Professor">Assistant Professor</option>
+<option value="Lecturer">Lecturer</option>
+</select>
+</td>
+</tr>
+<tr><td colspan="2"><input type="submit" value="ADD TO REGISTER" style="width:100%"></td></tr>
+</form>
+</table>
 
-<h2>Categories</h2>
-							<ul>
-								<li><a href="admin_home.php">admin Home</a></li>
-								
-								
-								
-								<?php if(!isset($_SESSION['facultyname'])){?>
-							    <li><a href="logout.php">Logout</a></li>
-								<li><a href="addfaculty.php">New Faculty Registration</a></li>
-								<?php } else{?>
-								<li><a href="user_account.php">My Account</a></li>
-								
-								<?php }?>
-								&nbsp;
-								
-							</ul>
 
-</body>
-</html>
+</td>
+</tr>
+</table> 
+ 
+ <?php include('footer.php');?>
